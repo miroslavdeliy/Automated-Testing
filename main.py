e7002736cd57f01acb69638795ef500eacdbd893
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-10 17:09:03 +0300

    1-st version

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ driver.get(base_url) #Открыть ссылку
 driver.set_window_size(1920, 1080) #Установить разрешение окна
 user_name = driver.find_element(By.XPATH, "//*[@id='user-name']") #Создание объекта поля ввода логина
 user_name.send_keys("standard_user") #Ввести данные в поле логина
+print('Input Login') #Вывод информации о вводе логина
 password = driver.find_element(By.XPATH, "//*[@id='password']") #Создание объекта поля ввода пароля
 password.send_keys("secret_sauce") #ввести данные в поле пароля
+print('Input Password') #Вывод информации о вводе пароля
 button_login = driver.find_element(By.XPATH, "//*[@id='login-button']") #Создание объекта кнопки входа
 button_login.click() #Нажать на кнопку
+print('Click Login Button') #Вывод информации о нажатии на кнопку
+get_url = driver.current_url #Сохранение информации о текущем url
+url = 'https://www.saucedemo.com/inventory.html' #Сохранение информации о целевом url
+assert url == get_url #Сравнение, что текущий url совпадает с целевым
+print('URL корректен')
+text_products = driver.find_element(By.XPATH, "//span[@class='title']") #Создание объекта Product
+value_text_products = text_products.text #Сохранение текста объекта Product
+assert value_text_products == 'Products' #Проверка, что на текущей странице есть объект с надписью "Product"
+print('Заголовок корректен')
