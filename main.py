22d480fc0d101ef7383d9ce178546425c1d86ce7
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-11 11:23:07 +0300

    1-st version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Импортирование вебдрайвера
+#Импортирование библиотек
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -7,24 +7,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install())) #Создание объекта driver для работы с Google Chrome
 base_url = 'https://www.saucedemo.com/' #Базовый URL
-driver.get(base_url) #Открыть ссылку
-driver.set_window_size(1920, 1080) #Установить разрешение окна
-user_name = driver.find_element(By.XPATH, "//*[@id='user-name']") #Создание объекта поля ввода логина
-user_name.send_keys("standard_user") #Ввести данные в поле логина
-print('Input Login') #Вывод информации о вводе логина
-password = driver.find_element(By.XPATH, "//*[@id='password']") #Создание объекта поля ввода пароля
-password.send_keys("secret_sauce") #ввести данные в поле пароля
-print('Input Password') #Вывод информации о вводе пароля
-button_login = driver.find_element(By.XPATH, "//*[@id='login-button']") #Создание объекта кнопки входа
-button_login.click() #Нажать на кнопку
-print('Click Login Button') #Вывод информации о нажатии на кнопку
-get_url = driver.current_url #Сохранение информации о текущем url
-url = 'https://www.saucedemo.com/inventory.html' #Сохранение информации о целевом url
-assert url == get_url #Сравнение, что текущий url совпадает с целевым
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+#Открыть окно браузера
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
+
+#Авторизация на сайте
+user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
+user_name.send_keys("standard_user")
+print('Input Login')
+password = driver.find_element(By.XPATH, "//*[@id='password']")
+password.send_keys("secret_sauce")
+print('Input Password')
+button_login = driver.find_element(By.XPATH, "//*[@id='login-button']")
+button_login.click()
+print('Click Login Button')
+
+#Проверка, что после авторизации находимся на целевом url
+get_url = driver.current_url
+url = 'https://www.saucedemo.com/inventory.html'
+assert url == get_url
 print('URL корректен')
-text_products = driver.find_element(By.XPATH, "//span[@class='title']") #Создание объекта Product
-value_text_products = text_products.text #Сохранение текста объекта Product
-assert value_text_products == 'Products' #Проверка, что на текущей странице есть объект с надписью "Product"
+
+#Проверка, что находимся на странице каталога
+text_products = driver.find_element(By.XPATH, "//span[@class='title']")
+value_text_products = text_products.text
+assert value_text_products == 'Products'
 print('Заголовок корректен')
