8c9dc30b5138f80e59658975adb277ecef950493
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-18 18:24:26 +0300

    Создание скриншота

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #Импортирование библиотек
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+import datetime
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -15,8 +14,9 @@ driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverMan
 #Открыть окно браузера
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
+print('Открыто окно браузера')
 
-#Ввести неверный логин
+#Ввести логин
 user_name = driver.find_element(By.XPATH, "//input[@id = 'user-name']")
 user_name.send_keys('standard_user')
 print('Введен логин')
@@ -26,16 +26,13 @@ password = driver.find_element(By.XPATH, "//input[@id = 'password']")
 password.send_keys('secret_sauce')
 print('Введен пароль')
 
-#Выделение полей логина и пароля и удаление их
-user_name.send_keys(Keys.CONTROL + 'a')
-time.sleep(2)
-user_name.send_keys(Keys.BACKSPACE)
-password.send_keys(Keys.CONTROL + 'a')
-time.sleep(2)
-password.send_keys(Keys.BACKSPACE)
-
 #Нажатие на кнопку авторизации
 button_click = driver.find_element(By.XPATH, "//input[@id = 'login-button']")
 button_click.click()
+print('Нажата кнопка авторизации')
 
-
+#Создание скриншота
+now_date = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
+name_screnshot = 'screenshot' + now_date + '.png'
+driver.save_screenshot('C:\\Users\\miros\\PycharmProjects\\Send_key\\screen\\' + name_screnshot)
+print('Скриншот сохранен')
