025bd8a3850b8f7b36960d8bd87dfc8700e54923
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-19 14:27:48 +0300

    Разлогирование

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #Импортирование библиотек
+import time
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -17,32 +16,26 @@ driver.get(base_url)
 driver.set_window_size(1920, 1080)
 print('Открыто окно браузера')
 
-#Ввести неправильный логин
+#Ввод данных авторизаци
 user_name = driver.find_element(By.XPATH, "//input[@id = 'user-name']")
-user_name.send_keys('standard_us')
-print('Введен неправильный логин')
-
-#Ввести неправильный пароль
-password = driver.find_element(By.XPATH, "//input[@id = 'password']")
-password.send_keys('secret_s')
-print('Введен неправильный пароль')
-
-#Выделение полей и удаление
-user_name.send_keys(Keys.CONTROL + 'a')
-user_name.send_keys(Keys.DELETE)
-password.send_keys(Keys.CONTROL + 'a')
-password.send_keys(Keys.DELETE)
-print('Поля очищены')
-
-#Ввод правильных данных
 user_name.send_keys('standard_user')
+password = driver.find_element(By.XPATH, "//input[@id = 'password']")
 password.send_keys('secret_sauce')
-print('Введены правильные данные')
+print('Введены данные')
 
 #Нажатие на кнопку авторизации
 button_click = driver.find_element(By.XPATH, "//input[@id = 'login-button']")
 button_click.click()
 print('Нажата кнопка авторизации')
 
+#Открытие меню
+menu_button = driver.find_element(By.XPATH, '//*[@id="react-burger-menu-btn"]')
+menu_button.click()
+print('Открыто меню')
+time.sleep(1)
 
+#Разлогирование
+logout_button = driver.find_element(By.XPATH, '//*[@id="logout_sidebar_link"]')
+logout_button.click()
+print('Разлогирование из системы')
 
