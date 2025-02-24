6b55474cfe9cb6907cf53d5a73ae0abe49a1d382
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-24 19:52:38 +0300

    Чек-бок

---
+++ mail.py
@@ -0,0 +1,22 @@
+#Импортирование библиотек
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+#Открытие окна браузера
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://demoqa.com/checkbox'
+driver.get(base_url)
+driver.set_window_size(1920,1080)
+
+#Выбрать чек бокс
+check_box = driver.find_element(By.XPATH, "//span[@class='rct-checkbox']")
+check_box.click()
+
+#Проверка, что выбран
+check_box.is_selected()
+print('Чек-бокс выбран')
