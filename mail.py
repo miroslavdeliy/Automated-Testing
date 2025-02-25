a46547020e35cb91703b144b2db1cdce331bb3d2
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-25 16:08:48 +0300

    Двойной и правый клик

--- mail.py
+++ mail.py
@@ -1,5 +1,6 @@
 #Импортирование библиотек
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,16 +10,28 @@ options.add_experimental_option("detach", True)
 
 #Открытие окна браузера
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://demoqa.com/radio-button'
+base_url = 'https://demoqa.com/buttons'
 driver.get(base_url)
 driver.set_window_size(1920,1080)
 
-#Выбрать radio-button
-radio_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div[2]/div[3]/label')
-radio_button.click()
+action = ActionChains(driver)
 
-#Проверка, что выбран radio-button
-radio_button_input = driver.find_element(By.XPATH, '//*[@id="impressiveRadio"]')
-assert radio_button_input.is_selected(), 'Ошибка! radio-button не выбран!'
-print('radio-button выбран')
+#Двойной клик
+double_click_button = driver.find_element(By.XPATH, '//*[@id="doubleClickBtn"]')
+action.double_click(double_click_button).perform()
+print('Произведен двойной клик')
 
+#Проверка, что был произведен двойной клик
+double_click_button_message = driver.find_element(By.XPATH, '//*[@id="doubleClickMessage"]').text
+assert double_click_button_message == 'You have done a double click', 'Ошибка! Двойной клик не был совершен!'
+print('Двойной клик был совершен')
+
+#Правая кнопка мыши
+right_click_button = driver.find_element(By.XPATH, '//*[@id="rightClickBtn"]')
+action.context_click(right_click_button).perform()
+print('Произвели клик правой кнопки мыши')
+
+#Проверка, что был произведен клик правой кнопкой
+right_click_button_message = driver.find_element(By.XPATH, '//*[@id="rightClickMessage"]').text
+assert right_click_button_message == 'You have done a right click', 'Ошибка! Клик правой кнопкой не был совершен!'
+print('Клик правой кнопкой был совершен')
