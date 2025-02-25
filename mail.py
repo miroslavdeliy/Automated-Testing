cb1455e9fa3d90856bc3a3126fdb7da6392bd6b9
Deliy Miroslav <miroslavdely@gmail.com>
2025-02-25 14:43:59 +0300

    Радио-баттон

--- mail.py
+++ mail.py
@@ -9,15 +9,16 @@ options.add_experimental_option("detach", True)
 
 #Открытие окна браузера
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://demoqa.com/checkbox'
+base_url = 'https://demoqa.com/radio-button'
 driver.get(base_url)
 driver.set_window_size(1920,1080)
 
-#Выбрать чек бокс
-check_box = driver.find_element(By.XPATH, '//span[@class="rct-checkbox"]')
-check_box.click()
+#Выбрать radio-button
+radio_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div[2]/div[3]/label')
+radio_button.click()
 
-#Проверка, что выбран
-check_box_input = driver.find_element(By.XPATH, '//input[@type="checkbox"]')
-assert check_box_input.is_selected(), 'Ошибка! Чек-бокс не выбран!'
-print('Чек-бокс выбран')
+#Проверка, что выбран radio-button
+radio_button_input = driver.find_element(By.XPATH, '//*[@id="impressiveRadio"]')
+assert radio_button_input.is_selected(), 'Ошибка! radio-button не выбран!'
+print('radio-button выбран')
+
