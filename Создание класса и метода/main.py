c8b60e573052743102c36ef56c9e349f50691558
Deliy Miroslav <miroslavdely@gmail.com>
2025-03-06 16:41:16 +0300

    2-я версия

--- Создание класса и метода/main.py
+++ Создание класса и метода/main.py
@@ -1,20 +1,18 @@
 # Импортирование библиотек и классов
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
 
 # Создание класса для тестирования
 class Test():
     # Конструктор класса
     def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
     # Метод открытия браузера
-    def test_select_product(self):
+    def open_browser(self):
         base_url = 'https://www.saucedemo.com/'
         self.driver.get(base_url)
         self.driver.set_window_size(1920,1080)
@@ -23,4 +21,4 @@ class Test():
 
 # Создание экземпляра класса и выполнение методов
 start_test = Test()
-start_test.test_select_product()
+start_test.open_browser()
