0075d49d63a92d72b397bba62e4f595ed73bf984
Deliy Miroslav <miroslavdely@gmail.com>
2025-03-06 15:16:44 +0300

    1-я версия

---
+++ Создание класса и метода/main.py
@@ -0,0 +1,37 @@
+# Импортирование библиотек и классов
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+# Создание класса для тестирования
+class Test():
+    # Конструктор класса
+    def __init__(self):
+        self.driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+    # Метод открытия браузера
+    def test_select_product(self):
+        base_url = 'https://www.saucedemo.com/'
+        self.driver.get(base_url)
+        self.driver.set_window_size(1920,1080)
+
+    # Метод авторизации
+    def authorization(self, login, password):
+        username = self.driver.find_element(By.XPATH, '//*[@id="user-name"]')
+        username.send_keys(login)
+        password_input = self.driver.find_element(By.XPATH, '//*[@id="password"]')
+        password_input.send_keys(password)
+        button_login = self.driver.find_element(By.XPATH, '//*[@id="login-button"]')
+        button_login.click()
+
+
+# Создание экземпляра класса и выполнение методов
+start_test = Test()
+start_test.test_select_product()
+print('Браузер открылся')
+start_test.authorization('standard_user', 'secret_sauce')
+print('Пользователь авторизовался')
